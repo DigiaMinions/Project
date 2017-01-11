32ebf61590e14edd4b6948c3c8c2c197c3efae55
Aleksi Vuorela <H8346@LABRANET.JAMK.FI>
2017-01-11 17:32:52 +0200

    Node testing

--- Scripts/startnode.sh
+++ Scripts/startnode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000
 cd /home/ec2-user
-sudo npm install express
-sudo npm install forever
+sudo npm install -g express
+sudo npm install -g forever
 sudo killall node
 forever start server.js
