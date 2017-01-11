c76d5b9d016880b7ff439732c98db7435f418f48
Aleksi Vuorela <H8346@LABRANET.JAMK.FI>
2017-01-11 17:03:32 +0200

    Node testing

--- Scripts/installnode.sh
+++ Scripts/installnode.sh
@@ -2,4 +2,3 @@
 sudo su
 curl -sL https://rpm.nodesource.com/setup_4.x | bash -
 yum -y install nodejs
-npm install express
--- Scripts/startnode.sh
+++ Scripts/startnode.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000
-node /home/ec2-user/server.js
+cd /home/ec2-user
+npm install express
+node server.js
