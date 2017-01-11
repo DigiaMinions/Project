65f3899b061143a293796c43e8a268e89f3597ae
Aleksi Vuorela <H8346@LABRANET.JAMK.FI>
2017-01-11 16:44:17 +0200

    Node testing

--- Scripts/installnode.sh
+++ Scripts/installnode.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo yum -y install nodejs npm --enablerepo=epel
+sudo su
+curl -sL https://rpm.nodesource.com/setup_4.x | bash -
+yum install nodejs
+npm install express
+iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000
+node /home/ec2-user/server.js
