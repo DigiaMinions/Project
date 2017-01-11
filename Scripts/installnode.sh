ba134056ca624a2246dbb42204756fd4f9c1960d
Aleksi Vuorela <H8346@LABRANET.JAMK.FI>
2017-01-11 12:35:15 +0200

    Node fixes

--- Scripts/installnode.sh
+++ Scripts/installnode.sh
@@ -2,6 +2,3 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | b
 . ~/.nvm/nvm.sh
 nvm install 4.4.5
 npm install express
-sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000
-sudo killall -9 node
-node /home/ec2-user/server.js
