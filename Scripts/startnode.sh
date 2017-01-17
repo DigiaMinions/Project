919d5d8394b814a8e378ca75629aee11a0ca4870
Aleksi Vuorela <H8346@LABRANET.JAMK.FI>
2017-01-17 14:26:50 +0200

    Fix scripts

--- Scripts/startnode.sh
+++ Scripts/startnode.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sudo su # run as root
 iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000 # redirect port 80 (http) to 9000 (node)
 cd /home/ec2-user # home folder
-sudo npm install -g # install all node dependencies globally
-sudo killall node # kill existing node processes
+npm install # install all node dependencies
+killall node # kill existing node processes
 npm run dev # run scripts (webpack bundle) in dev mode
 forever start ./src/server.js # start node via forever
