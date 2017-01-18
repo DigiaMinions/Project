25b242878aac9babb9aed06ee2022bff57830826
Aleksi Vuorela <H8346@Labranet.jamk.fi>
2017-01-18 13:02:44 +0200

    Fix scripts

--- Scripts/installnode.sh
+++ Scripts/installnode.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-sudo su # run as root
-curl -sL https://rpm.nodesource.com/setup_4.x | bash - # install node via script
+#!/bin/sh
+curl -sL https://rpm.nodesource.com/setup_4.x | bash - # run script to get node package
 yum -y install nodejs # install node via yum
--- Scripts/startnode.sh
+++ Scripts/startnode.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-sudo su # run as root
+#!/bin/sh
 iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000 # redirect port 80 (http) to 9000 (node)
 cd /home/ec2-user # home folder
 npm install # install all node dependencies
