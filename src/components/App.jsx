689fa1650e24e20a62e75f36200b22eb1d5019f3
Aleksi Vuorela <aleksii.vuorela@gmail.com>
2017-01-23 12:47:06 +0200

    Test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { Grid, Row, Col, Button, Alert } from 'react-bootstrap';
 import { device } from 'aws-iot-device-sdk';
 
 
+
 export default class App extends React.Component {
 
 	constructor(props) {
