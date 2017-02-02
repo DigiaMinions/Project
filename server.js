5d9f29beb53f35b69d538c01a8859cb154c29efe
prome <janne.mottola@hotmail.com>
2017-02-02 14:56:17 +0200

    Add redirects and login page

--- server.js
+++ server.js
@@ -20,29 +20,47 @@ app.use(bodyParser.json()); // JSON body
 var router = express.Router();
 
 /*Passport*/
-var passport = require('passport')
+var passport = require('passport');
 passport.initialize();
 var LocalStrategy = require('passport-local').Strategy;
 
-app.get('/login', function(req,res){
-	res.sendFile(__dirname + '/src/static/login.html');
-});
 
-app.get('/', loggedIn, function(req, res){
-	
+app.get('/', loggedIn, function (req, res){	
   res.sendFile(__dirname + '/src/static/index.html'); // tyhjä pyyntö -> lähetetään /static/index.html
 });
 
+app.get('/aikataulu', loggedIn, function (req, res){	
+  res.sendFile(__dirname + '/src/static/index.html'); // lähetetään /static/index.html
+});
+
+app.get('/login', function (req, res){
+	res.sendFile(__dirname + '/src/static/login.html');
+});
+
+app.get('/js/login.js', function (req, res) {
+  res.sendFile(__dirname + '/src/static/js/login.js');
+});
+
+app.get('/css/login.css', function (req, res) {
+  res.sendFile(__dirname + '/src/static/css/login.css');
+});
+
+app.get('/css/style.css', function (req, res) {
+  res.sendFile(__dirname + '/src/static/css/style.css');
+});
+
+app.get('/js/bundle.js', function (req, res) {
+  res.sendFile(__dirname + '/src/static/js/bundle.js');
+});
+
+app.get('*', function (req,res){
+	res.sendFile(__dirname + '/src/static/index.html');
+});
+
 // HUOM! julkisten filujen antaminen tultava api.gettien jälkeen, jotta ohjaus toimii oikein.
-app.use(express.static(__dirname + '/src/static')); // pyyntö julkiseen static-kansioon -> lähetetään pyydetty tiedosto kansiosta
+//app.use(express.static(__dirname + '/src/static')); // pyyntö julkiseen static-kansioon -> lähetetään pyydetty tiedosto kansiosta
 
 var users = [ 'test' ];
-
-
-// Redirect ei toimi tässä.
-// res.render tarvitsee viewit mitä heitellä
-
-// Joko siis monta .html filua tai sitten siirrytään view engineen ja vieweihin
 
 // EI server side renderöintiä käyttöön, koska
 // http://stackoverflow.com/questions/27290354/reactjs-server-side-rendering-vs-client-side-rendering
@@ -50,8 +68,6 @@ var users = [ 'test' ];
 // For multi-page apps you can add multiple entry points (one per page) to your webpack config:
 // https://webpack.github.io/docs/multiple-entry-points.html
 
-
-// joko css ei toimi tai ohjaus ei toimi
 
 function loggedIn(req, res, next) {
     //if (req.user) {
@@ -62,13 +78,7 @@ function loggedIn(req, res, next) {
     	console.log("User ei ok, uudelleenohjataan.");
         res.redirect('/login');
     }
-
 };
-
-
-
-/* client voi pyytää tiedostoja vain kahdesta yllä olevasta paikasta */
-/* eli jos pyydetään esim. /server/secureFile.js -> ei tee yhtikäs mitään */
 
 /* API endpointit */
 /* Insta feed */
