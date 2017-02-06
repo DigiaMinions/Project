81d01259375bd9fa6880511bf30180b60f9a1b2e
prome <janne.mottola@hotmail.com>
2017-02-06 18:48:31 +0200

    Update mysql create commands and remove test inserts

--- Feeder test insert commands.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO User(email, pass, salt) VALUES ('test@test.test', 'test', 'test');
-INSERT INTO Device(mac, name, FK_user_id) VALUES ('test', 'test', (SELECT id FROM User WHERE email = 'test@test.test'));
-INSERT INTO Template(name, FK_user_id) VALUES ('testtemplate', (SELECT ID FROM User WHERE email = 'test@test.test'));
-INSERT INTO Schedule(feedingtime, days, FK_template_id) VALUES ('08:00:00', 3, (SELECT id FROM Template WHERE name = 'testtemplate'));
