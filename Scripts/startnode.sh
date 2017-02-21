7382b14e82f5c7914b9270e90244a5b720b1f183
Aleksi Vuorela <aleksii.vuorela@gmail.com>
2017-02-21 18:58:50 +0200

    Fix

--- Scripts/startnode.sh
+++ Scripts/startnode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000 # redirect port 80 (http) to 9000 (node)
-cd /home/ec2-user
+cd /home/ec2-user/release
 npm install --production # install node dependencies locally
 npm install forever -g # install forever globally
 killall node # kill existing node processes
