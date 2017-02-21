8345bff7128047ea6ce6ca192c4b95dfc65ae3f4
Aleksi Vuorela <aleksii.vuorela@gmail.com>
2017-02-21 19:48:42 +0200

    CodeDeploy testing

--- Scripts/startnode.sh
+++ Scripts/startnode.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000 # redirect port 80 (http) to 9000 (node)
 cd /home/ec2-user
-cp -R .env release/ # copy credentials to release folder
-cp -R credentials.env release/ # copy credentials to release folder
+cp -R .env release/ # copy .env to release folder
 cd /home/ec2-user/release
 npm install --production # install node dependencies locally
 npm install forever -g # install forever globally
