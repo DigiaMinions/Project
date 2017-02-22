4b7f922c2d04d63da62b771639fc4ecc11907841
Aleksi Vuorela <aleksii.vuorela@gmail.com>
2017-02-22 19:44:57 +0200

    Fix dropdown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -54,7 +54,7 @@ export default class App extends React.Component {
 						<Col xs={12} md={3}>
 							<div>
 								Valitse laite:
-								<Dropdown options={this.state.userDevices} onChange={this.onSelect} value={this.state.activeDevice} placeholder={this.state.activeDevice.label} />
+								<Dropdown options={this.state.userDevices} onChange={this.onSelect} placeholder={this.state.activeDevice.label} />
 							</div>
 						</Col>
 						<Col xs={12} md={9}>
