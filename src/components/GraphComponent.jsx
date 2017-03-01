a8b2363b248d586e6d802d0a7d72bfe93a80c5d6
prome <janne.mottola@hotmail.com>
2017-03-01 13:56:12 +0200

    Change grafana ip for graphcomponent

--- src/components/GraphComponent.jsx
+++ src/components/GraphComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const graphanaUrl = "http://34.249.41.238:3000/dashboard-solo/db/newdashboard?panelId=1&var-mac=";
+const graphanaUrl = "http://172.31.31.13:3000/dashboard-solo/db/newdashboard?panelId=1&var-mac=";
 const theme = "&theme=light";
 
 export default class GraphComponent extends React.Component {
